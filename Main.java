a45973f19371b6a8e3819f2190c283262b0d23d5
tccmobile <William.smith@tulsacc.edu>
2024-10-28 15:30:30 +0000

    Updates to loops

--- Main.java
+++ Main.java
@@ -3,11 +3,37 @@ public class Main {
         // Create some shapes
         Circle circle = new Circle(0, 0, 5, "red");
         Rectangle rectangle = new Rectangle(2, 3, 4, 6, "blue");
+        Shape[] shapes = new Shape[2];
+        Drawable[] drawables = new Drawable[2];
         
+        drawables[0] = circle;
+        drawables[1] = rectangle;
+
+        System.out.println("First Demo - Drawable Array\n\n");
+        for (Drawable drawable : drawables) {
+            drawable.draw();
+            drawable.setColor("green");
+            System.out.println("Color: " + drawable.getColor());
+         //   System.out.println("Area: " + drawable.calculateArea()); Not possible
+        }
+
+        System.out.println("\n\nNext Demo - Shape Array\n\n");
+        shapes[0] = circle;
+
+        shapes[1] = rectangle;
+
+
+        for (Shape shape : shapes) {
+            shape.draw();
+            System.out.println("Area: " + shape.calculateArea());
+        }
+
+        System.out.println("\n\nOriginal work\n\n");
         // Use interface methods
         circle.draw();
         rectangle.draw();
-        
+       
+
         // Use abstract class methods
         System.out.println("Circle area: " + circle.calculateArea());
         System.out.println("Rectangle area: " + rectangle.calculateArea());
